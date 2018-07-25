626d2d2112db3842742a7491a6adca68eb7bd08b
Beauty Mess <33240287+BeautyMess@users.noreply.github.com>
2018-07-25 17:38:35 +0800

    Update common_ops.py
    增加了注释

--- src/common_ops.py
+++ src/common_ops.py
@@ -1,9 +1,17 @@
+# -*- coding: utf-8 -*
+#应该是包含了一些通用的操作
+#
+#
+#create_weight()用来初始化权重
+#create_bias()用来初始化偏置
 import numpy as np
 import tensorflow as tf
 
 
 def lstm(x, prev_c, prev_h, w):
-  ifog = tf.matmul(tf.concat([x, prev_h], axis=1), w)
+  ifog = tf.matmul(tf.concat([x, prev_h], axis=1), w)  #将x和prev_h拼合，并乘以w
+  #ifog=[x,prev_h]*w,x和prev_h是以增广矩阵方式相连，即x的行数等于prev_h的行数
+  #按列将ifog分成4份
   i, f, o, g = tf.split(ifog, 4, axis=1)
   i = tf.sigmoid(i)
   f = tf.sigmoid(f)
@@ -26,12 +34,14 @@ def stack_lstm(x, prev_c, prev_h, w):
 
 def create_weight(name, shape, initializer=None, trainable=True, seed=None):
   if initializer is None:
+	#使用keras接口，调用He正态分布初始化方法初始化权重
     initializer = tf.contrib.keras.initializers.he_normal(seed=seed)
   return tf.get_variable(name, shape, initializer=initializer, trainable=trainable)
 
 
 def create_bias(name, shape, initializer=None):
   if initializer is None:
+	#偏置初始化为0.0
     initializer = tf.constant_initializer(0.0, dtype=tf.float32)
   return tf.get_variable(name, shape, initializer=initializer)
 
