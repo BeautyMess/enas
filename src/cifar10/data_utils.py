b0d1d4e25bf9bdcaa9d292730ab8143c9e47ad5b
Beauty Mess <33240287+BeautyMess@users.noreply.github.com>
2018-07-27 17:00:13 +0800

    Update data_utils.py
    增加了注释

--- src/cifar10/data_utils.py
+++ src/cifar10/data_utils.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+#用来读取CIFAR-10数据集中的images和labels，read_data()通过调用私有函数_read_data()实现读取数据的功能
 import os
 import sys
 import cPickle as pickle
@@ -22,10 +24,10 @@ def _read_data(data_path, train_files):
       batch_labels = np.array(data["labels"], dtype=np.int32)
       images.append(batch_images)
       labels.append(batch_labels)
-  images = np.concatenate(images, axis=0)
-  labels = np.concatenate(labels, axis=0)
-  images = np.reshape(images, [-1, 3, 32, 32])
-  images = np.transpose(images, [0, 2, 3, 1])
+  images = np.concatenate(images, axis=0)           #所有图像样本聚合成一个长串的list
+  labels = np.concatenate(labels, axis=0)           #所有标签聚合成一个长串的list
+  images = np.reshape(images, [-1, 3, 32, 32])      #把图像reshape为[N,C,H,W]
+  images = np.transpose(images, [0, 2, 3, 1])       #把[N,C,H,W]转换为[N,H,W,C]
 
   return images, labels
 
@@ -46,8 +48,9 @@ def read_data(data_path, num_valids=5000):
   test_file = [
     "test_batch",
   ]
-  images["train"], labels["train"] = _read_data(data_path, train_files)
+  images["train"], labels["train"] = _read_data(data_path, train_files) #使用私有函数
 
+  #从训练集中抽取一部分作为验证集，数量为num_valids
   if num_valids:
     images["valid"] = images["train"][-num_valids:]
     labels["valid"] = labels["train"][-num_valids:]
@@ -57,19 +60,25 @@ def read_data(data_path, num_valids=5000):
   else:
     images["valid"], labels["valid"] = None, None
 
+  #读取测试集
   images["test"], labels["test"] = _read_data(data_path, test_file)
 
+  
   print "Prepropcess: [subtract mean], [divide std]"
+  #计算每个通道上的均值
   mean = np.mean(images["train"], axis=(0, 1, 2), keepdims=True)
+  #计算每个通道上的标准差
   std = np.std(images["train"], axis=(0, 1, 2), keepdims=True)
 
   print "mean: {}".format(np.reshape(mean * 255.0, [-1]))
   print "std: {}".format(np.reshape(std * 255.0, [-1]))
 
+  """数据归一化"""
   images["train"] = (images["train"] - mean) / std
   if num_valids:
     images["valid"] = (images["valid"] - mean) / std
   images["test"] = (images["test"] - mean) / std
+  """数据归一化"""
 
   return images, labels
 
